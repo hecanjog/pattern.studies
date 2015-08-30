6a572336991c4e65abafdc5821d630a13558f7c2
Erik Schoster <erik@hecanjog.com>
2015-08-30 16:45:31 -0500

    shift long_chord down an octave

--- orc/rhodes.py
+++ orc/rhodes.py
@@ -6,23 +6,23 @@ key = 'g'
 rhodes = snds.load('hcj/rhodes1.wav')
 rhodes = dsp.transpose(rhodes, 16.0/15.0)
 
-def chord(length, c, amp):
-    layers = [ keys.rhodes(length, freq, amp * dsp.rand(0.25, 0.5)) for freq in c ]
+def chord(length, freqs, amp):
+    layers = [ keys.rhodes(length, freq, amp * dsp.rand(0.25, 0.5)) for freq in freqs ]
     layers = [ dsp.pan(layer, dsp.rand()) for layer in layers ]
 
     return dsp.mix(layers)
 
 def makeStab(length, i):
-    c = tune.fromdegrees([ dsp.randchoose([1,4,5,8]) for _ in range(dsp.randint(2,4)) ], octave=3, root=key)
-    stab = chord(length, c, dsp.rand(0.5, 0.75))
+    freqs = tune.fromdegrees([ dsp.randchoose([1,4,5,8]) for _ in range(dsp.randint(2,4)) ], octave=3, root=key)
+    stab = chord(length, freqs, dsp.rand(0.5, 0.75))
     stab = dsp.taper(stab, 40)
     stab = dsp.fill(stab, length, silence=True)
 
     return stab
 
 def makePulse(length, i):
-    c = tune.fromdegrees([ dsp.randchoose([1,4,5,8]) for _ in range(dsp.randint(2,4)) ], octave=2, root=key)
-    pulse = chord(length, c, dsp.rand(0.5, 0.75)) 
+    freqs = tune.fromdegrees([ dsp.randchoose([1,4,5,8]) for _ in range(dsp.randint(2,4)) ], octave=2, root=key)
+    pulse = chord(length, freqs, dsp.rand(0.5, 0.75)) 
     pulse = dsp.taper(pulse, 40)
     pulse = dsp.amp(pulse, dsp.rand(0.9, 1.5))
     pulse = dsp.fill(pulse, length, silence=True)
@@ -30,8 +30,8 @@ def makePulse(length, i):
     return pulse
 
 def makeLongChord(seg):
-    c = [ dsp.randint(1, 9) for _ in range(dsp.randint(2,4)) ]
-    long_chord = chord(sum(seg), tune.fromdegrees(c, octave=dsp.randint(2,4), root=key), dsp.rand(0.6, 0.75))
+    degrees = [ dsp.randint(1, 9) for _ in range(dsp.randint(2,4)) ]
+    long_chord = chord(sum(seg), tune.fromdegrees(degrees, octave=dsp.randint(1,3), root=key), dsp.rand(0.6, 0.75))
     long_chord = dsp.fill(long_chord, sum(seg))
 
     return long_chord
