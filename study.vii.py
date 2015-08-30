50d82f26064c0339749e1eb5de9845ecf862ccec
Erik Schoster <erik@hecanjog.com>
2015-08-30 17:52:09 -0500

    First pass prob map for section sequencing

--- study.vii.py
+++ study.vii.py
@@ -12,67 +12,94 @@ key = 'g'
 
 out = ''
 changeindex = 0
-segs = ctl.tempoPath(50)
+nsegs = 50
+
+segs = ctl.tempoPath(nsegs)
+
+kickprob = [ dsp.rand() for _ in range(nsegs) ]
+snareprob = [ dsp.rand() for _ in range(nsegs) ]
+hatprob = [ dsp.rand() for _ in range(nsegs) ]
+guitarprob = [ 0 for _ in range(nsegs) ]
+stabprob = [ dsp.rand() for _ in range(nsegs) ]
+pulseprob = [ dsp.rand() for _ in range(nsegs) ]
+longchordprob = [ dsp.rand() for _ in range(nsegs) ]
+longguitarprob = [ dsp.rand() for _ in range(nsegs) ]
+glitchprob = [ dsp.rand() for _ in range(nsegs) ]
 
 for segi, seg in enumerate(segs): 
     print 'Rendering section %s' % (segi + 1)
 
-    # kicks
-    kickp =  'x...-.....x..x...'
-    pattern = ctl.parseBeat(kickp)
-    kicks = ctl.makeBeat(pattern, seg, orc.kick.make)
+    layers = []
 
-    # snares
-    snarep = '..x...x...'
-    pattern = ctl.parseBeat(snarep)
-    subseg = ctl.splitSeg(seg, 2)
-    snares = ctl.makeBeat(pattern, subseg, orc.snare.make)
+    if dsp.rand() < kickprob[segi]:
+        kickp =  'x...-.....x..x...'
+        pattern = ctl.parseBeat(kickp)
+        kicks = ctl.makeBeat(pattern, seg, orc.kick.make)
 
-    # hats
-    hatp =   'xxxx'
-    pattern = ctl.parseBeat(hatp)
-    subseg = ctl.splitSeg(seg, 4)
-    hats = ctl.makeBeat(pattern, subseg, orc.hat.make)
+        layers += [ kicks ]
 
-    # guitar 
-    pattern = ctl.parseBeat('x  x')
-    orc.suiteguitar.scale = orc.suiteguitar.makeScale()
-    guitars = ctl.makeBeat(pattern, seg, orc.suiteguitar.make)
+    if dsp.rand() < snareprob[segi]:
+        snarep = '..x...x...'
+        pattern = ctl.parseBeat(snarep)
+        subseg = ctl.splitSeg(seg, 2)
+        snares = ctl.makeBeat(pattern, subseg, orc.snare.make)
 
-    # stabs
-    bar_length = dsp.randint(4, 13)
-    num_pulses = dsp.randint(1, bar_length)
-    orc.rhodes.key = key
-    pattern = dsp.eu(bar_length, num_pulses)
-    pattern = dsp.rotate(pattern, vary=True)
-    subseg = ctl.splitSeg(seg, 3)
-    stabs = ctl.makeBeat(pattern, subseg, orc.rhodes.makeStab)
+        layers += [ snares ]
+
+    if dsp.rand() < hatprob[segi]:
+        hatp =   'xxxx'
+        pattern = ctl.parseBeat(hatp)
+        subseg = ctl.splitSeg(seg, 4)
+        hats = ctl.makeBeat(pattern, subseg, orc.hat.make)
+
+        layers += [ hats ]
+
+    if dsp.rand() < guitarprob[segi]:
+        pattern = ctl.parseBeat('x  x')
+        orc.suiteguitar.scale = orc.suiteguitar.makeScale()
+        guitars = ctl.makeBeat(pattern, seg, orc.suiteguitar.make)
+
+        layers += [ guitars ]
+
+    if dsp.rand() < stabprob[segi]:
+        bar_length = dsp.randint(4, 13)
+        num_pulses = dsp.randint(1, bar_length)
+        orc.rhodes.key = key
+        pattern = dsp.eu(bar_length, num_pulses)
+        pattern = dsp.rotate(pattern, vary=True)
+        subseg = ctl.splitSeg(seg, 3)
+        stabs = ctl.makeBeat(pattern, subseg, orc.rhodes.makeStab)
+
+        layers += [ stabs ]
     
-    # pulses
-    pulsep = 'x..'
-    pattern = ctl.parseBeat(pulsep)
-    pulses = ctl.makeBeat(pattern, seg, orc.rhodes.makePulse)
+    if dsp.rand() < pulseprob[segi]:
+        orc.rhodes.key = key
+        pulsep = 'x..'
+        pattern = ctl.parseBeat(pulsep)
+        pulses = ctl.makeBeat(pattern, seg, orc.rhodes.makePulse)
 
-    instLayers = [ kicks, snares, stabs, hats, pulses ]
+        layers += [ pulses ]
 
-    if segi <= 40:
-        for _ in range(0, 4):
-            instLayers.pop(dsp.randint(0, len(instLayers)-1))
-
-    section = dsp.mix(instLayers)
-
+    orc.rhodes.key = key
     long_chord = orc.rhodes.makeLongChord(seg)
-    long_guitar = orc.guitar.makeLong(seg)
+    if dsp.rand() < longchordprob[segi]:
+        layers += [ long_chord ]
 
-    subseg = ctl.splitSeg(seg, 2)
-    orc.rhodes.long_chord = long_chord
-    glitches = ctl.makeBeat([1,1], subseg, orc.rhodes.makeGlitch)
+    if dsp.rand() < longguitarprob[segi]:
+        long_guitar = orc.guitar.makeLong(seg)
+        layers += [ long_guitar ]
+
+    if dsp.rand() < glitchprob[segi]:
+        subseg = ctl.splitSeg(seg, 2)
+        orc.rhodes.long_chord = long_chord
+        glitches = ctl.makeBeat([1,1], subseg, orc.rhodes.makeGlitch)
+
+        layers += [ glitches ]
 
     changeindex = changeindex + 1
 
-    section = dsp.mix([ section, long_chord, long_guitar, glitches ])
-
-    out += section
-
+    if len(layers) > 0:
+        section = dsp.mix(layers)
+        out += section
 
 dsp.write(out, 'study.vii')
