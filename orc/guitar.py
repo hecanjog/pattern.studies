08a72308218bdcd3d1189894446ecd8e6e7dc5a8
Erik Schoster <erik@hecanjog.com>
2015-09-03 12:21:43 -0500

    Adjust long rhodes octave range and add fade guitar

--- orc/guitar.py
+++ orc/guitar.py
@@ -1,10 +1,17 @@
 from pippi import dsp
+from hcj import fx
 
 guitars = [ dsp.read('samples/guitar%s.wav' % (i + 1)).data for i in range(5) ]
 
 def makeLong(seg):
     g = dsp.randchoose(guitars)
-    g = dsp.amp(g, 0.5)
+    g = dsp.amp(g, 0.4)
     long_guitar = dsp.fill(g, sum(seg))
 
-    return long_guitar
+    fade_guitar = dsp.randchoose(guitars)
+    fade_guitar = dsp.amp(fade_guitar, dsp.rand(0.1, 0.3))
+    fade_guitar = fx.penv(fade_guitar)
+
+    out = dsp.mix([ long_guitar, fade_guitar ])
+
+    return out
