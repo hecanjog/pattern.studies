9507b482702177cd41c3e464e4de059a993a6af0
Erik Schoster <erik@hecanjog.com>
2015-09-11 22:41:34 -0500

    add chord sequences to X

--- study.x.py
+++ study.x.py
@@ -1,35 +1,38 @@
 from pippi import dsp, tune
 
 out = ''
-layers = []
 
+nchords = 8
 npulses = 500
 nlayers = 3
 
-for _ in range(nlayers):
-    layer = ''
-    highs = dsp.breakpoint([ dsp.rand(60, 15000) for _ in range(npulses / 100) ], npulses)
-    lows = [ dsp.rand(20, freq) for freq in highs ]
-    amps = dsp.breakpoint([ dsp.rand(0.1, 0.3) for _ in range(npulses / 100) ], npulses)
-    pans = dsp.breakpoint([ dsp.rand(0, 1) for _ in range(npulses / 10) ], npulses)
-    lengths = [ dsp.mstf(l) for l in dsp.breakpoint([ dsp.rand(80, 400) for _ in range(npulses / 10) ], npulses) ]
+for _ in range(nchords):
+    layers = []
 
-    for high, low, amp, pan, length in zip(highs, lows, amps, pans, lengths):
-        p = dsp.bln(length, low, high, wform=dsp.randchoose(['hann', 'sine2pi', 'tri']))
-        p = dsp.env(p, 'hann')
-        p = dsp.taper(p, 20)
-        p = dsp.amp(p, amp)
-        p = dsp.pan(p, pan)
+    for _ in range(nlayers):
+        layer = ''
+        highs = dsp.breakpoint([ dsp.rand(60, 15000) for _ in range(npulses / 100) ], npulses)
+        lows = [ dsp.rand(20, freq) for freq in highs ]
+        amps = dsp.breakpoint([ dsp.rand(0.1, 0.3) for _ in range(npulses / 100) ], npulses)
+        pans = dsp.breakpoint([ dsp.rand(0, 1) for _ in range(npulses / 10) ], npulses)
+        lengths = [ dsp.mstf(l) for l in dsp.breakpoint([ dsp.rand(1, 30) for _ in range(npulses / 10) ], npulses) ]
 
-        layer += p
+        for high, low, amp, pan, length in zip(highs, lows, amps, pans, lengths):
+            p = dsp.bln(length, low, high, wform=dsp.randchoose(['hann', 'sine2pi', 'tri']))
+            p = dsp.env(p, 'hann')
+            p = dsp.taper(p, 20)
+            p = dsp.amp(p, amp)
+            p = dsp.pan(p, pan)
 
-    layers += [ layer ]
+            layer += p
 
-freqs = tune.fromdegrees([1,5,6], octave=2, root='e')
+        layers += [ layer ]
 
-for i, freq in enumerate(freqs):
-    layers[i] = dsp.pine(layer, dsp.flen(layer) * 10, freq)
+    freqs = tune.fromdegrees([dsp.randint(1, 9) for _ in range(nlayers)], octave=2, root='e')
 
-out = dsp.mix(layers)
+    for i, freq in enumerate(freqs):
+        layers[i] = dsp.pine(layer, dsp.flen(layer) * 10, freq)
+
+    out += dsp.mix(layers)
 
 dsp.write(out, '04-study.x')
