1e48c2cf870c04476bfbf4dccda409398374be2f
Erik Schoster <erik@hecanjog.com>
2015-09-11 22:36:24 -0500

    Harmonize X layers

--- study.x.py
+++ study.x.py
@@ -1,12 +1,9 @@
 from pippi import dsp, tune
 
 out = ''
-
-npulses = 1000
-lengths = [ dsp.mstf(l) for l in dsp.breakpoint([ dsp.rand(80, 400) for _ in range(npulses / 10) ], npulses) ]
-
 layers = []
 
+npulses = 500
 nlayers = 3
 
 for _ in range(nlayers):
@@ -15,6 +12,7 @@ for _ in range(nlayers):
     lows = [ dsp.rand(20, freq) for freq in highs ]
     amps = dsp.breakpoint([ dsp.rand(0.1, 0.3) for _ in range(npulses / 100) ], npulses)
     pans = dsp.breakpoint([ dsp.rand(0, 1) for _ in range(npulses / 10) ], npulses)
+    lengths = [ dsp.mstf(l) for l in dsp.breakpoint([ dsp.rand(80, 400) for _ in range(npulses / 10) ], npulses) ]
 
     for high, low, amp, pan, length in zip(highs, lows, amps, pans, lengths):
         p = dsp.bln(length, low, high, wform=dsp.randchoose(['hann', 'sine2pi', 'tri']))
@@ -26,6 +24,11 @@ for _ in range(nlayers):
         layer += p
 
     layers += [ layer ]
+
+freqs = tune.fromdegrees([1,5,6], octave=2, root='e')
+
+for i, freq in enumerate(freqs):
+    layers[i] = dsp.pine(layer, dsp.flen(layer) * 10, freq)
 
 out = dsp.mix(layers)
 
